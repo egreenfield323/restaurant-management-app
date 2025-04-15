644acd3a88223aaa17020983e2781f7dc0a6b964
Evan Greenfield <evangreenfield@live.com>
2025-04-14 22:35:02 -0400

    add update links to every row

--- Project/restaurant-app/src/components/HomePage.vue
+++ Project/restaurant-app/src/components/HomePage.vue
@@ -3,24 +3,18 @@
     <h1>Hello {{ this.name }}, welcome to the home page.</h1>
     <table border="1px">
         <tr>
-            <td>
-                Id
-            </td>
-            <td>
-                Name
-            </td>
-            <td>
-                Contact
-            </td>
-            <td>
-                Address
-            </td>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Contact</td>
+            <td>Address</td>
+            <td>Actions</td>
         </tr>
         <tr v-for="item in restaurant" :key="item.id">
             <td>{{ item.id }}</td>
             <td>{{ item.name }}</td>
             <td>{{ item.contact }}</td>
             <td>{{ item.address }}</td>
+            <td><router-link :to="'/update/' + item.id">Update</router-link></td>
         </tr>
     </table>
 </template>
