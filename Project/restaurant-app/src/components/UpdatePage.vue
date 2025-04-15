94aeb53e2e30335f709168ec7936ba101a354cf5
Evan Greenfield <evangreenfield@live.com>
2025-04-14 22:35:39 -0400

    grab user input

--- Project/restaurant-app/src/components/UpdatePage.vue
+++ Project/restaurant-app/src/components/UpdatePage.vue
@@ -1,6 +1,12 @@
 <template>
     <PageHeader />
     <h1>Welcome to the update restaurant page.</h1>
+    <form class="add">
+        <input type="text" name="name" placeholder="Enter name" v-model="restaurant.name" />
+        <input type="text" name="address" placeholder="Enter address" v-model="restaurant.address" />
+        <input type="text" name="contact" placeholder="Enter contact" v-model="restaurant.contact" />
+        <button type="button" v-on:click="updateRestaurant">Update</button>
+    </form>
 </template>
 
 <script>
@@ -9,6 +15,15 @@ export default {
     name: 'UpdatePage',
     components: {
         PageHeader
+    },
+    data() {
+        return {
+            restaurant: {
+                name: '',
+                address: '',
+                contact: ''
+            }
+        }
     },
     mounted() {
         let user = localStorage.getItem('user-info');
