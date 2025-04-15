7fc04b9199e49193e01b5ca7dc1bb189b047191c
Evan Greenfield <evangreenfield@live.com>
2025-04-14 21:22:47 -0400

    implement login api

--- Project/restaurant-app/src/components/LogIn.vue
+++ Project/restaurant-app/src/components/LogIn.vue
@@ -4,7 +4,7 @@
     <div class="login">
         <input type="text" v-model="email" placeholder="Enter Email" />
         <input type="password" v-model="password" placeholder="Enter Password" />
-        <button>Log In</button>
+        <button v-on:click="login">Log In</button>
         <p>
             Already a user?
             <router-link to="/sign-up">Login</router-link>
@@ -14,9 +14,36 @@
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
-    name: 'LogIn'
-}
+    name: 'LogIn',
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async login() {
+            let result = await axios.get(
+                `http://localhost:3000/users?email=${this.email}&password=${this.password}`
+            )
+            if (this.email != '' && this.password != '' && result.status === 200 && result.data.length > 0) {
+                result.data.forEach(element => {
+                    if (element.email === this.email && element.password === this.password) {
+                        localStorage.setItem("user-info", JSON.stringify(result.data[0]))
+                        this.$router.push({ name: 'HomePage' });
+                    }
+                });
+            }
+        }
+    },
+    mounted() {
+        let user = localStorage.getItem('user-info');
+        if (user) {
+            this.$router.push({ name: 'HomePage' });
+        }
+    }
+};
 
 </script>
