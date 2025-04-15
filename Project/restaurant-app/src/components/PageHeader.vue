b2baf50fcb1061244c419ae564f0cbd09f70581c
Evan Greenfield <evangreenfield@live.com>
2025-04-14 21:41:17 -0400

    add logout functionality

--- Project/restaurant-app/src/components/PageHeader.vue
+++ Project/restaurant-app/src/components/PageHeader.vue
@@ -4,14 +4,20 @@
         <a href="#">Home</a>
         <a href="#">Add Restaurant</a>
         <a href="#">Update Restaurant</a>
-        <a href="#">Logout</a>
+        <a v-on:click="logout" href="#">Logout</a>
     </div>
 
 </template>
 
 <script>
 export default {
-    name: 'PageHeader'
+    name: 'PageHeader',
+    methods: {
+        logout() {
+            localStorage.clear();
+            this.$router.push({ name: 'LogIn' })
+        }
+    }
 }
 </script>
 
