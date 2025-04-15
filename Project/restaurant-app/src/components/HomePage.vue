d941f0284f7bf9a7eec5786ba788435c88ed30a1
Evan Greenfield <evangreenfield@live.com>
2025-04-14 21:34:51 -0400

    create route for PageHeader

--- Project/restaurant-app/src/components/HomePage.vue
+++ Project/restaurant-app/src/components/HomePage.vue
@@ -1,10 +1,15 @@
 <template>
+    <PageHeader />
     <h1>Welcome to the home page.</h1>
 </template>
 
 <script>
+import PageHeader from './PageHeader.vue';
 export default {
     name: 'HomePage',
+    components: {
+        PageHeader
+    },
     mounted() {
         let user = localStorage.getItem('user-info');
         if (!user) {
