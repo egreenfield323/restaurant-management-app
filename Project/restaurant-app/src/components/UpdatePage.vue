17461f0fed4b6412c2968f69c6646dd65c218b8d
Evan Greenfield <evangreenfield@live.com>
2025-04-14 22:44:00 -0400

    populate fields with restaurant values

--- Project/restaurant-app/src/components/UpdatePage.vue
+++ Project/restaurant-app/src/components/UpdatePage.vue
@@ -11,6 +11,7 @@
 
 <script>
 import PageHeader from './PageHeader.vue';
+import axios from 'axios';
 export default {
     name: 'UpdatePage',
     components: {
@@ -25,11 +26,14 @@ export default {
             }
         }
     },
-    mounted() {
+    async mounted() {
         let user = localStorage.getItem('user-info');
         if (!user) {
             this.$router.push({ name: 'SignUp' });
         }
+
+        const result = await axios.get('http://localhost:3000/restaurants/' + this.$route.params.id);
+        this.restaurant = result.data
     }
 }
 </script>
