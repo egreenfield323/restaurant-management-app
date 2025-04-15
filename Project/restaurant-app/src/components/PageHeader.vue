d53d7a251e23bfc613abaa388b53d1b83043659b
Evan Greenfield <evangreenfield@live.com>
2025-04-14 21:34:43 -0400

    Create PageHeader.vue

---
+++ Project/restaurant-app/src/components/PageHeader.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <div class="nav">
+        <a href="#">Home</a>
+        <a href="#">Add Restaurant</a>
+        <a href="#">Update Restaurant</a>
+        <a href="#">Logout</a>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'PageHeader'
+}
+</script>
+
+<style>
+.nav {
+    background-color: #333;
+    overflow: hidden;
+}
+
+.nav a {
+    float: left;
+    color: aliceblue;
+    padding: 14px 16px;
+    text-align: center;
+    font-size: 17px;
+    text-decoration: none;
+    margin-right: 6px;
+}
+
+.nav a:hover {
+    background-color: white;
+    color: #333;
+}
+</style>
