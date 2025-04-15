99bf30791baa939d5fd208383e2951741c3f0de2
Evan Greenfield <evangreenfield@live.com>
2025-04-14 22:08:47 -0400

    display restaurants in list

--- Project/restaurant-app/src/components/HomePage.vue
+++ Project/restaurant-app/src/components/HomePage.vue
@@ -1,26 +1,60 @@
 <template>
     <PageHeader />
     <h1>Hello {{ this.name }}, welcome to the home page.</h1>
+    <table border="1px">
+        <tr>
+            <td>
+                Id
+            </td>
+            <td>
+                Name
+            </td>
+            <td>
+                Contact
+            </td>
+            <td>
+                Address
+            </td>
+        </tr>
+        <tr v-for="item in restaurant" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.contact }}</td>
+            <td>{{ item.address }}</td>
+        </tr>
+    </table>
 </template>
 
 <script>
 import PageHeader from './PageHeader.vue';
+import axios from 'axios';
 export default {
     name: 'HomePage',
     data() {
         return {
-            name: ''
+            name: '',
+            restaurant: [],
         }
     },
     components: {
         PageHeader
     },
-    mounted() {
+    async mounted() {
         let user = localStorage.getItem('user-info');
         this.name = JSON.parse(user).name;
         if (!user) {
             this.$router.push({ name: 'SignUp' });
         }
+        let result = await axios.get('http://localhost:3000/restaurants');
+        this.restaurant = result.data;
+
     }
 }
 </script>
+
+<style>
+td {
+    width: 160px;
+    height: 40px;
+}
+</style>
