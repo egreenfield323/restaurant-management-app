1a145827dc2ca10408f397f22e995f06ab22f251
Evan Greenfield <evangreenfield@live.com>
2025-04-14 21:52:08 -0400

    Create UpdatePage.vue

---
+++ Project/restaurant-app/src/components/UpdatePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <PageHeader />
+    <h1>Welcome to the update restaurant page.</h1>
+</template>
+
+<script>
+import PageHeader from './PageHeader.vue';
+export default {
+    name: 'UpdatePage',
+    components: {
+        PageHeader
+    },
+    mounted() {
+        let user = localStorage.getItem('user-info');
+        if (!user) {
+            this.$router.push({ name: 'SignUp' });
+        }
+    }
+}
+</script>
