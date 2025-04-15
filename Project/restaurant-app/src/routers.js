df358fa2429e8fa7795efb7e6832b5fc82469832
Evan Greenfield <evangreenfield@live.com>
2025-04-14 21:52:20 -0400

    add routes to AddPage and UpdatePage

--- Project/restaurant-app/src/routers.js
+++ Project/restaurant-app/src/routers.js
@@ -1,6 +1,8 @@
 import HomePage from "./components/HomePage.vue";
 import LogIn from "./components/LogIn.vue";
 import SignUp from "./components/SignUp.vue";
+import AddPage from "./components/AddPage.vue";
+import UpdatePage from "./components/UpdatePage.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
@@ -18,6 +20,16 @@ const routes = [
     name: "LogIn",
     component: LogIn,
     path: "/log-in",
+  },
+  {
+    name: "AddPage",
+    component: AddPage,
+    path: "/add",
+  },
+  {
+    name: "UpdatePage",
+    component: UpdatePage,
+    path: "/update",
   },
 ];
 
