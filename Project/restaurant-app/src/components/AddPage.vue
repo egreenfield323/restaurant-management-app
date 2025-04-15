c601812fdf3dc93a0e2ccfd031563560a11e2283
Evan Greenfield <evangreenfield@live.com>
2025-04-14 21:51:41 -0400

    Create AddPage.vue

---
+++ Project/restaurant-app/src/components/AddPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <PageHeader />
+    <h1>Welcome to the add restaurant page.</h1>
+</template>
+
+<script>
+import PageHeader from './PageHeader.vue';
+export default {
+    name: 'AddPage',
+    components: {
+        PageHeader
+    },
+    mounted() {
+        let user = localStorage.getItem('user-info');
+        if (!user) {
+            this.$router.push({ name: 'SignUp' });
+        }
+    }
+}
+</script>
