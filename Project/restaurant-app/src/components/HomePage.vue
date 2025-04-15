7dc14b032ef30d5ffd86b9021654fe03a353593d
Evan Greenfield <evangreenfield@live.com>
2025-04-14 21:51:49 -0400

    make home page display username

--- Project/restaurant-app/src/components/HomePage.vue
+++ Project/restaurant-app/src/components/HomePage.vue
@@ -1,17 +1,23 @@
 <template>
     <PageHeader />
-    <h1>Welcome to the home page.</h1>
+    <h1>Hello {{this.name}}, welcome to the home page.</h1>
 </template>
 
 <script>
 import PageHeader from './PageHeader.vue';
 export default {
     name: 'HomePage',
+    data() {
+        return {
+            name: ''
+        }
+    },
     components: {
         PageHeader
     },
     mounted() {
         let user = localStorage.getItem('user-info');
+        this.name = JSON.parse(user).name;
         if (!user) {
             this.$router.push({ name: 'SignUp' });
         }
